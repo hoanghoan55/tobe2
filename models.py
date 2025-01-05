3988686f03d7f0e74aa604ddbe1ad3f11bd04ca7
hoanghoan55 <108189946+hoanghoan55@users.noreply.github.com>
2025-01-05 09:44:03 +0700

    Update models.py

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(512), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     is_active = db.Column(db.Boolean, default=True)
     date_joined = db.Column(db.DateTime, default=datetime.utcnow)
