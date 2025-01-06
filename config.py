ec780565c4c14afb3d8e30e1bbeab047a3cdd9c1
hoanghoan55 <108189946+hoanghoan55@users.noreply.github.com>
2025-01-06 14:12:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
+# Cấu hình Cloudinary   
+cloudinary.config( 
+    cloud_name = "dwkkugzxr", 
+    api_key = "668853365344795", 
+    api_secret = "668853365344795", # Click 'View API Keys' above to copy your API secret
+    secure=True
+)
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
